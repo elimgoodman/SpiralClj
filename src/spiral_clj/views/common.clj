0646d0380ee1a14dbfc9db1ecdca7b672448d2b5
Eli Goodman <eli.m.goodman@gmail.com>
2013-01-20 22:57:07 -0500

    moar

--- src/spiral_clj/views/common.clj
+++ src/spiral_clj/views/common.clj
@@ -1,21 +1,30 @@
 (ns spiral-clj.views.common
-  (:require [noir.server :as server])
+  (:require [noir.server :as server]
+            [ring.adapter.jetty :as jetty])
   (:use [noir.core :only [defpartial defpage]]
         [noir.response :only [json]]
         [hiccup.page :only [include-css html5 include-js]]))
 
 (def my-server (ref nil))
 
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello World"})
+
+(defn start-server []
+  (jetty/run-jetty #'handler {:port 4000 :join? false}))
+
 (defpage [:post "/run"] {:as body}
          (let [instances (:instances body)
                pages (:pages instances)
                partials (:partials instances)
                layouts (:layouts instances)]
-           (dosync (ref-set my-server (server/start 5000)))
+           (dosync (ref-set my-server (start-server)))
            (json {:success true})))
 
 (defpage [:post "/stop"] {:as body}
-         (dosync (alter my-server server/stop))
+         (dosync (alter my-server (fn [s] (.stop s))))
          (json {:success true}))
 
 (defpartial js [file]
